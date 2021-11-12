cc4046d7d54e5a0e739e737367aaec52d59249f3
Emilija Briedyte <em.bredyte@gmail.com>
2021-11-12 10:04:03 +0200

    theme provider added

--- src/components/styled/Global.ts
+++ src/components/styled/Global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from "styled-components";
+import { theme } from "../../App";
 
-export default createGlobalStyle`
+export default createGlobalStyle<{ theme: typeof theme }>`
     * {
         box-sizing: border-box;
         margin: 0;
@@ -8,8 +9,8 @@ export default createGlobalStyle`
     }
 
     body {
-        background: #FFFFFF;
-        color: #000000;
+        background: ${({ theme }) => theme.colors.background};
+        color:  ${({ theme }) => theme.colors.text};
         font-family: 'Manrope', sans-serif;
         font-weight: 700;
         margin: 0;
@@ -18,6 +19,7 @@ export default createGlobalStyle`
     h1 {
         font-size: 4rem;
         margin-bottom: 29px;
+        color: #000000;
     }
 
     li, h2 {
@@ -25,6 +27,7 @@ export default createGlobalStyle`
     }
 
     a {
-        color: #00adaa;
+        color:  ${({ theme }) => theme.colors.primary};
+        text-decoration: none;
     }
 `;
--- src/components/styled/OrderedList.styled.ts
+++ src/components/styled/OrderedList.styled.ts
@@ -7,7 +7,7 @@ interface Props {
 export const OrderedList = styled.ol`
   width: 100%;
   list-style: none;
-  counter-reset: tasks-counter;
+  counter-reset: steps-counter;
 `;
 
 export const OrderedItem = styled.li<Props>`
@@ -15,29 +15,29 @@ export const OrderedItem = styled.li<Props>`
   list-style-position: inside;
   margin-bottom: 20px;
   max-width: 100%;
-  counter-increment: tasks-counter;
+  counter-increment: steps-counter;
 
   h2 {
     box-shadow: 3px 3px 15px rgba(0, 0, 0, 0.161);
     width: 100%;
 
     &:before {
-      content: counter(tasks-counter);
+      content: counter(steps-counter);
       font-size: 36px;
       display: inline-block;
       width: 50px;
       height: 53px;
       text-align: center;
-      background: #00adaa;
+      background: ${({ theme }) => theme.colors.primary};
       background: linear-gradient(180deg, #00a9a7 0%, #00ba89 100%);
       color: white;
-      margin: 16px 30px 16px 36px;
+      margin: 16px 36px 16px 30px;
     }
 
     &:after {
       content: "";
       display: block;
-      background: #00adaa;
+      background: ${({ theme }) => theme.colors.primary};
       height: 2px;
       width: ${(props) =>
         props.statusLineWidth ? props.statusLineWidth : 200}px;
